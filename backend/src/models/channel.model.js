590039ff5a5d237672de986343e4dc96ed165188
nikita <nikita1999@gmail.com>
2026-03-11 01:10:10 +0530

    channel and comment model created

---
+++ backend/src/models/channel.model.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const channelSchema = new mongoose.Schema({
+  channelName: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  channelBanner: String,
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  subscribers: {
+    type: Number,
+    default: 0
+  },
+  videos: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Video'
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export const Channel = mongoose.model('Channel', channelSchema);
